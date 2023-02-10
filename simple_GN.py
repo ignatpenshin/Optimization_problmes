0fa07d8f0682848be85548808f68b51514b950d9
Ignat Penshin <61110400+ignatpenshin@users.noreply.github.com>
2023-02-10 16:09:29 +0300

    Add files via upload

--- simple_GN.py
+++ simple_GN.py
@@ -1,5 +1,4 @@
 import numpy as np
-from math import sqrt
 
 x_i = np.array([0.038, 0.194, 0.425, 0.626,	1.253, 2.500, 3.740])
 y_i = np.array([0.050, 0.127, 0.094, 0.2122, 0.2729, 0.2665, 0.3317])
@@ -15,7 +14,7 @@ beta = np.array([b1, b2])
 dr_db1 = lambda x: - x/(beta[1] + x)
 dr_db2 = lambda x: beta[0]*x/(beta[1] + x)**2
 
-ms = lambda: sqrt((last[0] - beta[0])**2 + (last[1] - beta[1])**2)
+ms = lambda: np.sqrt((last[0] - beta[0])**2 + (last[1] - beta[1])**2)
 
 rate = lambda x: (beta[0] * x) / (beta[1] + x)
 grad_r = lambda x_i: np.array([[dr_db1(x) for x in x_i], 
